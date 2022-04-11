2fcde242e7baac990e611be5c9dfb90337372e9a
Thai Nguyen <ducthaidev@gmail.com>
2022-04-11 10:33:37 +0700

    update

--- lua/config-plugins.lua
+++ lua/config-plugins.lua
@@ -37,9 +37,6 @@ null_ls.setup({
 })
 
 require("nvim-treesitter.configs").setup({
-	-- One of "all", "maintained" (parsers with maintainers), or a list of languages
-	ensure_installed = "maintained",
-
 	-- Install languages synchronously (only applied to `ensure_installed`)
 	sync_install = false,
 
